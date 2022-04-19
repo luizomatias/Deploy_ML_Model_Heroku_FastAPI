96c4a8723bc2cdb27ed0a7e801402c38f9df7577
Luiz Matias <luizomatias2@gmail.com>
2022-04-19 10:25:15 -0300

    move main to another folder and create test

---
+++ main.py
@@ -0,0 +1,71 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+import joblib
+from starter.starter.ml.data import process_data
+import pandas as pd
+
+# Import model, encoder and lb
+model = joblib.load('starter/model/model_v1.pkl')
+encoder = joblib.load('starter/model/encoder_v1.pkl')
+lb = joblib.load('starter/model/lb_v1.pkl')
+
+#categorical features
+
+cat_features = [
+    "workclass",
+    "education",
+    "marital_status",
+    "occupation",
+    "relationship",
+    "race",
+    "sex",
+    "native_country",
+]
+
+# Features to ingest the body from POST
+class Features(BaseModel):
+    age: int = 28
+    workclass: str = 'Private'
+    fnlgt: int = 338409
+    education: str = 'Bachelors'
+    education_num: int = 13
+    marital_status: str = 'Married-civ-spouse'
+    occupation: str = 'Prof-specialty'
+    relationship: str = 'Wife'
+    race: str = 'Black'
+    sex: str = 'Female'
+    capital_gain: int = 0
+    capital_loss: int = 0
+    hours_per_week: int  = 40
+    native_country: str = 'Cuba'
+
+
+app = FastAPI()
+
+
+@app.get("/")
+async def welcome_message():
+    return {"message": "Welcome to classification model on publicly available Census Bureau data! :)"}
+
+
+@app.post("/predictions")
+async def predict_model(features: Features):
+
+    data = pd.DataFrame(data = features.dict(by_alias=True), index=[0])
+
+    X, _, _, _ = process_data(
+                                data,
+                                categorical_features=cat_features,
+                                label=None,
+                                training=False,
+                                encoder=encoder,
+                                lb=lb)
+
+    predict = model.predict(X)
+
+    if predict[0] == 1:
+        predict = "Salary > 50k"
+    else:
+        predict = "Salary <= 50k"
+
+    return {'predict': predict}
---
+++ test_api.py
@@ -0,0 +1,33 @@
+from fastapi.testclient import TestClient
+from main import app, Features
+import json
+import logging
+
+client = TestClient(app)
+
+
+def test_api_get_root():
+
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.json()['message'] == "Welcome to classification model on publicly available Census Bureau data! :)"
+
+
+def test_post_request_target_salary_bigger_50k():
+
+    data = Features().dict(by_alias=True)
+    response = client.post("/predictions", json = data)
+    assert response.status_code == 200
+    assert response.json() == {'predict': 'Salary > 50k'}
+
+
+def test_post_request_target_salary_smaller_50k():
+
+    data = Features().dict(by_alias=True)
+    data['relationship'] = 'Not-in-family'
+    data['education'] = 'HS-grad'
+    data['workclass'] = 'Self-emp-inc'
+    data['hours_per_week'] = 13
+    response = client.post("/predictions", json = data)
+    assert response.status_code == 200
+    assert response.json() == {'predict': 'Salary <= 50k'}
