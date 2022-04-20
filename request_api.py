88b626322b3a587f1ca9088b33fdb9aea7a08621
Luiz Matias <luizomatias2@gmail.com>
2022-04-20 08:15:47 -0300

    creating screenshots and update request api

--- request_api.py
+++ request_api.py
@@ -6,5 +6,6 @@ import pandas as pd
 data = Features().dict(by_alias=True)
 response = requests.post("https://ml-deploying-fastapi.herokuapp.com/predictions", json = data)
 
-print(response.json())
-
+print(f'Status Post Request: {response.status_code}')
+print()
+print(f'Prediction: {response.json()}')
